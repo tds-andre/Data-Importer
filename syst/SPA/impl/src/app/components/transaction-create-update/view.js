675547576edf8b073f428a841a323bebc1dfd71c
tds-andre <tds.andre@gmail.com>
2015-09-23 20:47:15 -0300

    yj

--- syst/SPA/impl/src/app/components/transaction-create-update/view.js
+++ syst/SPA/impl/src/app/components/transaction-create-update/view.js
@@ -16,6 +16,10 @@ var app = app || {};
 			
 		},	
 
+		hash: null,
+
+		$steps: [],
+
 		template: _.template($('#transaction-create-update-template').html()),
 
 		// Core --------------------------------------------------------------------------- //
@@ -26,6 +30,17 @@ var app = app || {};
 		},		
 
 		render: function () {
+			var
+				$stepsContainer;
+
+			this.hash = Math.random();
+			 
+			$stepsContainer = $(".js-transaction-create-udpate-steps", this.$el).prop("id","js-transaction-create-udpate-steps-"+this.hash);
+			$(".js-transaction-create-udpate-actions",this.$el).prop("id","js-transaction-create-udpate-actions-"+this.hash);
+			$(". js-transaction-create-update-step",this.$steps).each(function(index, el){
+				$steps.push($(el).prop("id", "js-transaction-create-udpate-step-"+index+ "-"+this.hash));
+			});
+
 			this.$el.html(this.template());			
 			return this;			
 		},
